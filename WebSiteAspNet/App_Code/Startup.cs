42b352c2a57d849f51b35f51149f6140e26929e0
Umesh Kumar <mrumeshkumar@hotmail.com>
2019-04-26 17:33:11 +0530

    first commit

---
+++ WebSiteAspNet/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteAspNet.Startup))]
+namespace WebSiteAspNet
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
